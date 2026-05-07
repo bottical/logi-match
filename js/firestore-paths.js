5768d7d4b8fa3f8424972e57bf3e7dcd46bc0021
bottical <129402269+bottical@users.noreply.github.com>
2026-05-07 13:46:03 +0900

    Stabilize Firestore path compatibility layer

--- js/firestore-paths.js
+++ js/firestore-paths.js
@@ -1,46 +1,99 @@
 (function () {
+  function getDb(db) {
+    if (db) return db;
+    if (window.db) return window.db;
+    if (window.firebase) return firebase.firestore();
+    throw new Error('[firestore-paths] db is required');
+  }
+
   function assertClientId(clientId) {
     if (!clientId || typeof clientId !== 'string') {
       throw new Error('[firestore-paths] clientId is required');
     }
   }
 
-  function createFirestorePaths(db, clientId) {
-    if (!db) throw new Error('[firestore-paths] db is required');
-    assertClientId(clientId);
+  function assertWorkId(workId) {
+    if (!workId || typeof workId !== 'string') {
+      throw new Error('[firestore-paths] workId is required');
+    }
+  }
 
-    const clientRef = db.collection('clients').doc(clientId);
+  function clientRoot(clientId, db) {
+    assertClientId(clientId);
+    return getDb(db).collection('clients').doc(clientId);
+  }
+
+  function createFirestorePaths(db, clientId) {
+    const clientRef = clientRoot(clientId, db);
 
     return {
       client: () => clientRef,
+      clientRoot: () => clientRef,
       users: () => clientRef.collection('users'),
       workers: () => clientRef.collection('workers'),
       csvMappingCurrent: () => clientRef.collection('csvMapping').doc('current'),
       importBatches: () => clientRef.collection('importBatches'),
       inspectionWorks: () => clientRef.collection('inspectionWorks'),
-      inspectionWork: (workId) => clientRef.collection('inspectionWorks').doc(workId),
-      inspectionItems: (workId) => clientRef.collection('inspectionWorks').doc(workId).collection('items'),
+      inspectionWork: (workId) => {
+        assertWorkId(workId);
+        return clientRef.collection('inspectionWorks').doc(workId);
+      },
+      inspectionItems: (workId) => {
+        assertWorkId(workId);
+        return clientRef.collection('inspectionWorks').doc(workId).collection('items');
+      },
       scanLogs: () => clientRef.collection('scanLogs'),
       operationLogs: () => clientRef.collection('operationLogs'),
     };
   }
 
-
-  function byClientId(name, db, clientId, ...args) {
-    const fn = createFirestorePaths(db || firebase.firestore(), clientId)[name];
-    return fn(...args);
+  function users(clientId) {
+    return clientRoot(clientId).collection('users');
+  }
+  function workers(clientId) {
+    return clientRoot(clientId).collection('workers');
+  }
+  function csvMappingCurrent(clientId) {
+    return clientRoot(clientId).collection('csvMapping').doc('current');
+  }
+  function importBatches(clientId) {
+    return clientRoot(clientId).collection('importBatches');
+  }
+  function inspectionWorks(clientId) {
+    return clientRoot(clientId).collection('inspectionWorks');
+  }
+  function inspectionWork(clientId, workId) {
+    assertWorkId(workId);
+    return inspectionWorks(clientId).doc(workId);
+  }
+  function inspectionItems(clientId, workId) {
+    assertWorkId(workId);
+    return inspectionWork(clientId, workId).collection('items');
+  }
+  function scanLogs(clientId) {
+    return clientRoot(clientId).collection('scanLogs');
+  }
+  function operationLogs(clientId) {
+    return clientRoot(clientId).collection('operationLogs');
   }
 
   window.firestorePaths = {
     createFirestorePaths,
-    users: (clientId) => byClientId('users', null, clientId),
-    workers: (clientId) => byClientId('workers', null, clientId),
-    csvMappingCurrent: (clientId) => byClientId('csvMappingCurrent', null, clientId),
-    importBatches: (clientId) => byClientId('importBatches', null, clientId),
-    inspectionWorks: (clientId) => byClientId('inspectionWorks', null, clientId),
-    inspectionWork: (clientId, workId) => byClientId('inspectionWork', null, clientId, workId),
-    inspectionItems: (clientId, workId) => byClientId('inspectionItems', null, clientId, workId),
-    scanLogs: (clientId) => byClientId('scanLogs', null, clientId),
-    operationLogs: (clientId) => byClientId('operationLogs', null, clientId),
+
+    // Legacy compatibility
+    clientRoot,
+    client: {
+      workers,
+    },
+
+    users,
+    workers,
+    csvMappingCurrent,
+    importBatches,
+    inspectionWorks,
+    inspectionWork,
+    inspectionItems,
+    scanLogs,
+    operationLogs,
   };
 })();
