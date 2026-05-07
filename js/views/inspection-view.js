da093f0c895a41967ecb1af310a4b626200f8466
bottical <129402269+bottical@users.noreply.github.com>
2026-05-08 00:25:42 +0900

    Override button widths for inspection input bar on tablet+

--- js/views/inspection-view.js
+++ js/views/inspection-view.js
@@ -6,8 +6,8 @@ export async function renderInspectionView(container) {
   const workId = params.get('work_id');
 
   const src = workId
-    ? `./inspection.html?shell=1&v=20260507-4&work_id=${encodeURIComponent(workId)}`
-    : './inspection.html?shell=1&v=20260507-4';
+    ? `./inspection.html?shell=1&v=20260507-7&work_id=${encodeURIComponent(workId)}`
+    : './inspection.html?shell=1&v=20260507-7';
 
   return renderFrameView(container, src, '検品実行');
 }
